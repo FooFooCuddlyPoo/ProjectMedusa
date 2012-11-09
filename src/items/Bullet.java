b9ef9cae1e1e1633397b50fff22e842be07382b0
Scott <caterwauler0b5@gmail.com>
2012-11-09 22:23:03 +1300

    More work on shooting

--- src/items/Bullet.java
+++ src/items/Bullet.java
@@ -6,58 +6,56 @@ import characters.Hitbox;
 import characters.Sprite;
 
 public class Bullet {
-	private int x;
-	private int y;
-	private double xSpeed;
-	private double ySpeed;
-	
-	private Sprite img;
-	
-	private Hitbox h;
-	
-	private static final int speed = 10;
-	private static final int BULLET_WIDTH = 8;
-	private static final int BULLET_HEIGHT = 8;
-	
-	public Bullet(int origX, int origY, int destX, int destY){
-		x = origX;
-		y = origY;
-		
-	    double angle = Math.atan(((double)(destY - origY))/((double)(destX - origX)));
-	    
-	    xSpeed = (speed*Math.acos(angle));
-	    ySpeed = (speed*Math.asin(angle));
-		
-		img = new Sprite("Sprites/Bullet.png");
-		h = new Hitbox(x, y, BULLET_WIDTH, BULLET_HEIGHT);
-	}
-	
-	public void move(){
-		x += xSpeed;
-		y += ySpeed;
-		
-		h.setX(x);
-		h.setY(y);
-	}
+    private int x;
+    private int y;
+    private double xSpeed;
+    private double ySpeed;
 
-	public Hitbox getHitbox() {
-		return h;
-	}
+    private Sprite img;
 
-	public void setHitbox(Hitbox h) {
-		this.h = h;
-	}
+    private Hitbox h;
 
-	public int getX() {
-		return x;
-	}
+    private static final int speed = 10;
+    private static final int BULLET_WIDTH = 8;
+    private static final int BULLET_HEIGHT = 8;
 
-	public int getY() {
-		return y;
-	}
-	
-	public void draw(Graphics g){
-		img.draw(g, x, y, BULLET_WIDTH, BULLET_HEIGHT, 0, 0);
-	}
+    public Bullet(int origX, int origY, int destX, int destY) {
+        x = origX;
+        y = origY;
+
+        xSpeed = (destX - origX)/speed;
+        ySpeed = (destY - origY)/speed;
+
+        img = new Sprite("Sprites/Bullet.png");
+        h = new Hitbox(x, y, BULLET_WIDTH, BULLET_HEIGHT);
+    }
+
+    public void move() {
+        x += xSpeed;
+        y += ySpeed;
+
+        h.setX(x);
+        h.setY(y);
+    }
+
+    public Hitbox getHitbox() {
+        return h;
+    }
+
+    public void setHitbox(Hitbox h) {
+        this.h = h;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void draw(Graphics g) {
+        img.draw(g, x, y, BULLET_WIDTH, BULLET_HEIGHT, 0, 0);
+    }
 
 }
